55e394a2839bc22b22c83c529c7985d93413243d
IrinaLokhvytska <irinaramzaeva2@gmail.com>
2018-03-21 23:26:32 +0200

    Update numberRange.py

--- ElementaryTask/numberRange.py
+++ ElementaryTask/numberRange.py
@@ -1,12 +1,13 @@
 '''Print the list of numders, if sqrt of them less than input number'''
 import math
+import validation
 
 
 class NumberRange:
     def __init__(self, number):
         self.number = number
 
-    def __calculate_range(self):
+    def calculate_range(self):
         min_num = 1
         output = list()
         while math.sqrt(min_num) < int(self.number):
@@ -14,35 +15,19 @@ class NumberRange:
             min_num += 1
         return ', '.join(output)
 
-    def validation(self):
-        msg = ''
-        if self.__check_empty_value():
-            if self.__check_positive_numbers():
-                return self.__calculate_range()
-            else:
-                msg += 'The ' + self.number + ' is not positive integer \n'
-        else:
-            msg += 'The number can not be empty \n'
-        return msg
 
-    def __check_empty_value(self):
-        '''Check that input value isn't empty'''
-        validation = False
-        if self.number:
-            validation = True
-        return validation
+def validate_number(number):
+    check_functions = {
+       validation.check_empty_value: 1,
+       validation.check_integer: 1,
+       validation.check_number_more_zero: 1
+    }
+    for function, expect in check_functions.items():
+        valid, msg = function({'number': number})
+        if valid != expect:
+            return msg
+    number_range = NumberRange(number)
+    return number_range.calculate_range()
 
-    def __check_positive_numbers(self):
-        '''Check that input value can be converted to integer'''
-        validation = False
-        try:
-            int(self.number)
-        except ValueError:
-            return validation
-        if int(self.number) >= 0:
-            validation = True
-        return validation
-
-n = input('Enter the number: ')
-number_range = NumberRange(n)
-print(number_range.validation())
+number = input('Enter the number: ')
+print(validate_number(number))
